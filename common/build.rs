861f96106c7487604e8273b21d63742773564881
fangying <ying.fang@beyonca.com>
2022-08-14 02:03:23 +0800

    fix docker

--- common/build.rs
+++ common/build.rs
@@ -2,4 +2,9 @@ fn main() {
     let mut app_version = String::from(env!("CARGO_PKG_VERSION"));
     app_version = format!("v{}",app_version);
     println!("cargo:rustc-env=APP_VERSION={}", app_version);
+
+    tonic_build::configure()
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile(&["proto/server_status.proto"], &["proto"])
+        .unwrap();
 }
